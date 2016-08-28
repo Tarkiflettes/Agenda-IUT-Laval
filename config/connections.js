ae1faada416635ebeca67ad50a44a29532efdb3e
Lionel Jouin <lionel.jouin@outlook.com>
2016-08-28 17:38:13 +0200

    configuration sails

--- config/connections.js
+++ config/connections.js
@@ -56,14 +56,14 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  // someMongodbServer: {
-  //   adapter: 'sails-mongo',
-  //   host: 'localhost',
-  //   port: 27017,
-  //   user: 'username', //optional
-  //   password: 'password', //optional
-  //   database: 'your_mongo_db_name_here' //optional
-  // },
+  someMongodbServer: {
+    adapter: 'sails-mongo',
+    host: 'ds017776.mlab.com',
+    port: 17776,
+    user: process.env.mongodb_user, //optional
+    password: process.env.mongodb_password, //optional
+    database: process.env.mongodb_database //optional
+  },
 
   /***************************************************************************
   *                                                                          *
--- config/env/production.js
+++ config/env/production.js
@@ -17,22 +17,22 @@ module.exports = {
    * environment (see config/connections.js and config/models.js )           *
    ***************************************************************************/
 
-  // models: {
-  //   connection: 'someMysqlServer'
-  // },
+  models: {
+    connection: 'someMysqlServer'
+  },
 
   /***************************************************************************
    * Set the port in the production environment to 80                        *
    ***************************************************************************/
 
-  // port: 80,
+  port: process.env.PORT || 1337,
 
   /***************************************************************************
    * Set the log level in production environment to "silent"                 *
    ***************************************************************************/
 
-  // log: {
-  //   level: "silent"
-  // }
+  log: {
+    level: "debug"
+  }
 
 };
