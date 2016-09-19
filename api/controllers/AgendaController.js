6dc91b832c757fdfdab46d71f122a9d03bccbf25
Lionel Jouin <lionel.jouin@outlook.com>
2016-09-19 22:16:55 +0200

    ajout API cours

--- api/controllers/AgendaController.js
+++ api/controllers/AgendaController.js
@@ -27,20 +27,20 @@ module.exports = {
       var output = [];
 
       sails.ical.fromURL(url, {}, function(err, data) {
-        
+
         var i = 0;
-        
+
         AgendaService.eventColor("lapin");
 
         for (var k in data){
           if (data.hasOwnProperty(k)) {
             var ev = data[k];
-            
+
             //console.log(ev);
-                     
+
             var start = sails.moment(ev.start);
             start = start.tz('Europe/Paris').format();
-            
+
             var end = sails.moment(ev.end);
             end = end.tz('Europe/Paris').format();
 
@@ -52,16 +52,62 @@ module.exports = {
               start: start,
               end: end,
               color: '#2980b9',
-              //color: '#'+AgendaService.eventColor(ev.summary),
+                //color: '#'+AgendaService.eventColor(ev.summary),
               url: '/e/'+id+"/"+i
             };
-            
+
             output.push(eventJ);
-            
+
             i++;
           }
         }
         res.json(output);
+      });
+    }
+  },
+
+  course: function(req, res) {
+
+    var id = req.param('id');
+
+    var idCours = req.param('idcours');
+
+    if (typeof id == 'undefined') {
+      res.badRequest("id non defini");
+    } else if (typeof idCours == 'undefined') {
+      res.badRequest("id cours non defini");
+    } else {
+      var url = 'http://edt.univ-lemans.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?resources='+id+'&projectId=2&calType=ical&nbWeeks=4';
+
+      var output = [];
+
+      sails.ical.fromURL(url, {}, function(err, data) {
+
+        AgendaService.eventColor("lapin");
+
+        var ev = data[Object.keys(data)[idCours]];
+        
+        if (typeof ev == 'undefined') {
+          res.badRequest("id cours n'existe pas");
+        } else {
+          var start = sails.moment(ev.start);
+          start = start.tz('Europe/Paris').format();
+
+          var end = sails.moment(ev.end);
+          end = end.tz('Europe/Paris').format();
+
+          var description = ev.description.replace(/(\(Exported :(?:.*)\))/g, "");
+
+          output = {
+            title: ev.summary,
+            description: description,
+            location: ev.location,
+            start: start,
+            end: end,
+          };
+      
+          res.json(output);
+        }
       });
     }
   }
