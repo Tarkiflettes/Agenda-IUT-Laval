c80ee7c53164563e7a95dd0a931bc1a2d3c9ba85
Lionel Jouin <lionel.jouin@outlook.com>
2016-09-27 10:23:00 +0000

    optimisation client

--- assets/app/common/sidenavController.js
+++ assets/app/common/sidenavController.js
@@ -8,8 +8,16 @@ var SidenavController = agendaIUTLaval.controller("SidenavController", function(
   $scope.grade = false;
   $scope.dep = true;
   $scope.cla = [];
-
+  $scope.currentId = 1;
+/*
   $http.get("/api/agenda/", {params:{"type": 0}})
+    .success(function(data){
+
+      vm.department = data;
+
+    });
+*/
+  $http.get("/api/agenda/", {params: {"limit": 1000}})    
     .success(function(data){
 
       vm.department = data;
@@ -24,6 +32,7 @@ var SidenavController = agendaIUTLaval.controller("SidenavController", function(
   $scope.changeMenu = function (id){
     $scope.dep = !$scope.dep;
     $scope.grade = !$scope.grade;
+    $scope.currentId = id;
     if($scope.grade) {
       $http.get("/api/agenda", {params: {"level": 2, "department": id, "limit": 1000}})
         .success(function(data){
